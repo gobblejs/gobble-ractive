fd2a1a41beaf2d27bdd2ff62ae931f2b35f68b3a
Rich Harris <richard.a.harris@gmail.com>
2015-01-19 13:59:12 -0500

    use external rcu-builders module

--- builders/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	amd: require( './amd' ),
-	cjs: require( './cjs' ),
-	es6: require( './es6' )
-};
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var rcu = require( 'rcu' );
+var builders = require( 'rcu-builders' );
 rcu.init( require( 'ractive' ) );
 
 module.exports = ractive;
 
 function ractive ( source, options ) {
-	var builders = require( './builders' ), builder = builders[ options.type || 'amd' ];
+	var builder = builders[ options.type || 'amd' ];
 
 	if ( !builder ) {
 		throw new Error( 'Cannot convert Ractive component to "' + options.type + '". Supported types: ' + Object.keys( builders ) );
